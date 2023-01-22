c09e05f5fbfaad9d725071d0ee418a811ede9d05
Cenzi76 <109979419+Cenzi76@users.noreply.github.com>
2023-01-22 17:48:41 -0500

    Update SwitchSolver.py

--- SwitchSolver.py
+++ SwitchSolver.py
@@ -1,9 +1,20 @@
 #Plus values:
-start = input("enter your current number: )
-finish = input("enter desired number: )
-  if start > finish:
-    finish += 10
-    #count is the plus value, like poker
-		count = finish - start
-  else :
-    count = finish - start
+begin = input("enter your current number: ")
+beginarr = [*begin]
+end = input("enter desired number: ")
+endarr = [*end]
+
+def Plusvalue(start,finish)
+    for i in start and finish
+        num1 = int(start[i])
+        num2 = int(finish[i])
+        if num1 > num2:
+            num2 += 10
+            #count is the plus value, like poker
+            count = num2 - num1
+        elif num1 < num2:
+            count = num2 - num1
+        else:
+            count = 0
+            
+print(Plusvalue(beginarr,endarr))

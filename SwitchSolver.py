bb1ddbf7d3d41dd278e4d13310149ef7810db67c
Cenzi76 <109979419+Cenzi76@users.noreply.github.com>
2023-01-22 17:18:58 -0500

    Create SwitchSolver.py

---
+++ SwitchSolver.py
@@ -0,0 +1,9 @@
+#Plus values:
+start = input("enter your current number: )
+finish = input("enter desired number: )
+  if start > finish:
+    finish += 10
+    #count is the plus value, like poker
+		count = finish - start
+  else :
+    count = finish - start
